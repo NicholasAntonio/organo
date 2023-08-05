8e88220dd2ff0bc0141a858131c827c6a6f10e7a
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-08-05 13:23:22 -0300

    inital commit

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,17 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria }
+const Time = ({time,colaboradores, aoDeletar}) => {
+    const css = { backgroundColor: time.corSecundaria }
 
     return (
-        (props.colaboradores.length > 0) ? <section className='time' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        (colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+                {colaboradores.map( colaborador => {
+
+                    return <Colaborador corDeFundo={time.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={aoDeletar}/>
+                } )}
             </div>
         </section> 
         : ''
