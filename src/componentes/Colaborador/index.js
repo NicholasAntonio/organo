f2c3e1d14a8038fffed6748c9f96959dbed98aaf
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-08-18 09:25:16 -0300

    favorite button

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,14 @@
 import './colaborador.css'
-import {BsFillTrashFill} from 'react-icons/bs'
+import {BsFillTrashFill, AiFillHeart, AiOutlineHeart} from 'react-icons/bs'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id)
+    }
+    const propsFavorito = {
+        size:25,
+        onclick: favoritar
+    }
     return (<div className="colaborador">
         <BsFillTrashFill size={25} className='deletar' onClick={()=> aoDeletar(colaborador.id)}></BsFillTrashFill>
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
@@ -10,6 +17,9 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito ? <AiFillHeart {...propsFavorito} color='#ff0000'/> : <AiOutlineHeart {...propsFavorito}/>}
+            </div>
         </div>
     </div>)
 }
